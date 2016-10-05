1a972b6efac90228ae15c118e9f09b99c02ac8af
kevintyk <kevintyk@users.noreply.github.com>
2016-10-05 21:04:19 +0800

    Create png2pdf.sh

---
+++ png2pdf.sh
@@ -0,0 +1,8 @@
+#!/bin/csh
+
+set dir=`ls -F ~/Downloads/ | grep / | sed -e "s/\///"`
+
+foreach x ($dir)
+        convert -limit memory 1 -limit map 1 ~/Downloads/$x/*.png ~/Downloads/$x/$x.pdf
+        cp ~/Downloads/$x/$x.pdf ~/
+end
