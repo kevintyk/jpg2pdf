16f5a936c4ebc6294a8e2c6d74fc42fd44e8486e
kevintyk <kevintyk@users.noreply.github.com>
2016-10-10 10:12:31 +0800

    Update img2pdf.sh

--- img2pdf.sh
+++ img2pdf.sh
@@ -2,7 +2,7 @@
 
 set dir=`ls -F ~/Downloads/ | grep / | sed -e "s/\///"`
 
-foreach x ($dir)
+foreach x ("$dir")
         convert -limit memory 1 -limit map 1 ~/Downloads/"$x"/"*.{png,jpg,jpeg}" ~/Downloads/"$x"/"$x".pdf
         cp ~/Downloads/"$x"/"$x".pdf ~/
 end
